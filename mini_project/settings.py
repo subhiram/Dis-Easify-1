4850ca7eaca3da39ecb2efb3375cb50030edf9bc
subhiram <71543749+subhiram@users.noreply.github.com>
2022-12-04 13:56:50 +0530

    Update settings.py

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-brqyr0ku_bjkq&9!#&aq1_az=s_%dtjna%4@e%ta2qi%9zkg2a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mini_app1',
 ]
 
 MIDDLEWARE = [
